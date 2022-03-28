5ab451232cdce6753d5019c31fb685de95803ef5
teniolao <Tennybetty100@gmail.com>
2022-03-28 13:27:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import babyNamesData from "./babyNamesData.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {babyNamesData
+        .sort((a, b) => a.name - b.name)
+        .map((value, index) => {
+          return (
+            <p key={index} className={value.sex}>
+              {value.name}
+            </p>
+          );
+        })}
     </div>
   );
 }
